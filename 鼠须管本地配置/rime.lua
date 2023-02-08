bf1faa552e141aa2295b95546fab0c5846a06dae
gaohang <wuyan12580@gmail.com>
2023-02-08 14:17:50 +0800

    2023-02-08 14:17:34

--- 鼠须管本地配置/rime.lua
+++ 鼠须管本地配置/rime.lua
@@ -29,6 +29,7 @@ function date_translator(input, seg)
         local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
         yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
         yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "礼拜"..weakTab[tonumber(os.date("%w")+1)], ""))
     end
 end
 
